dcb6562caf3866039d3f9dd6f2299c8fa6205c6a
Richard Gigan <richard.gigan@fr.clara.net>
2022-02-15 09:12:53 +0100

    {feat] - correction theme tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,53 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts}",
   ],
-  darkMode: "class",
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white' : 'rgba(255,255,255,0.8)'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
-      typography: (theme) => ({
-        dark: {
-          css: [
-            {
-              color: theme("colors.gray.400"),
-              '[class~="lead"]': {
-                color: theme("colors.gray.300"),
-              },
-              a: {
-                color: theme("colors.white"),
-              },
-              strong: {
-                color: theme("colors.white"),
-              },
-              "ol > li::before": {
-                color: theme("colors.gray.400"),
-              },
-              "ul > li::before": {
-                backgroundColor: theme("colors.gray.600"),
-              },
-              hr: {
-                borderColor: theme("colors.gray.200"),
-              },
-              blockquote: {
-                color: theme("colors.gray.200"),
-                borderLeftColor: theme("colors.gray.600"),
-              },
-              h1: {
-                color: theme("colors.white"),
-              },
-              h2: {
-                color: theme("colors.white"),
-              },
-              h3: {
-                color: theme("colors.white"),
-              },
-              h4: {
-                color: theme("colors.white"),
-              },
-              "figure figcaption": {
-                color: theme("colors.gray.400"),
-              },
-              code: {
-                color: theme("colors.white"),
-              },
-              "a code": {
-                color: theme("colors.white"),
-              },
-              pre: {
-                color: theme("colors.gray.200"),
-                backgroundColor: theme("colors.gray.800"),
-              },
-              thead: {
-                color: theme("colors.white"),
-                borderBottomColor: theme("colors.gray.400"),
-              },
-              "tbody tr": {
-                borderBottomColor: theme("colors.gray.600"),
-              },
-            },
-          ],
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }      
+    }
+  },
+  plugins: [
+    plugin(({ addUtilities })=> {
+      addUtilities({
+        'button': {
+          'background-color': '#ffffff',
+          borderColor : '#fafafa'
         },
-      }),
-    },
-  },
-  variants: {
-    extend: {
-      typography: ["dark"],
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+        
+      })
+    })
+  ]
+
 }
